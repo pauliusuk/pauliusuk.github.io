e6457644f1a7b402cc69745bb9e33fcc9cd3de6f
paulobox <152565276+PauloGitbox@users.noreply.github.com>
2024-03-26 23:09:45 +0000

    26marchMON

--- js/lazy-load.js
+++ js/lazy-load.js
@@ -1,9 +1,9 @@
 // static/js/lazy-load.js
 document.addEventListener("DOMContentLoaded", function() {
-    if (window.location.pathname === "/games/drum-kit.html") {
-        var link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = "/css/drum-kit.css";
-        document.head.appendChild(link);
-    }
+  if (window.location.pathname === "/games/drum-kit.html" || window.location.pathname === "/games/memory-game.html") {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "/css/drum-kit.css";
+    document.head.appendChild(link);
+  }
 });
